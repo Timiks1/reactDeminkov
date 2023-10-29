760eae0cec03838fe1b50c8bbef8ad95c523db2d
Timiks1 <prozor2@ukr.net>
2023-10-29 17:19:43 +0200

    Initial commit

---
+++ src/components/PilotInfo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>{{ pilot.name }}</h3>
+    <p>Height: {{ pilot.height }}</p>
+    <p>Mass: {{ pilot.mass }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pilot: Object, // Принимаем объект пилота
+  },
+};
+</script>
